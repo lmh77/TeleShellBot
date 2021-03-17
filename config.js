c967cc7d85a5e08639abf380ac4ea28611502488
lmh77 <62322755+lmh77@users.noreply.github.com>
2021-03-17 17:02:08 +0800

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ module.exports = {
     config:function(){
         return (
             {
-                adminUsers:[process.env.adminid], //admin users' telegram id, should be numbers
+                adminUsers:process.env.adminid, //admin users' telegram id, should be numbers
                 botToken: process.env.bot, // bot token
 
             }
